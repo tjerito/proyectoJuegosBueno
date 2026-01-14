bfc232e27d86511a13910655a94448e6882456c1
tjerito <fernandetejeroivan@gmail.com>
2026-01-14 10:58:31 +0100

    Initial commit

---
+++ src/main/java/com/example/proyectoJuegos/ProyectoJuegosApplication.java
@@ -0,0 +1,13 @@
+package com.example.proyectoJuegos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoJuegosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoJuegosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/proyectoJuegos/ProyectoJuegosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proyectoJuegos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoJuegosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
